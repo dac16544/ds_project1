8fa3589df5b224cfcad136b5f24d1023a4bc3250
dac16544 <dac16544@uga.edu>
2016-03-01 17:12:34 -0500

    made some cpp files

---
+++ LinkedList.cpp
@@ -0,0 +1,29 @@
+#include <string>
+
+using namespace std;
+
+Node::Node(Node * prev, Node * next, string category, ArrayQueue &orderQueue){
+	
+}	
+
+
+void LinkedList::append(string category, ArrayQueue &orderQueue){
+		
+}
+
+
+ArrayQueue & LinkedList::getQueueByCat(string cat){
+		
+}
+
+void LinkedList::setOrderCount(string cat, int count) {
+		
+}
+    
+const int LinkedList::size() const{
+		
+}
+	
+const bool LinkedList::empty() const {
+	return size() == 0;
+} // empty
---
+++ SuperMarket.cpp
@@ -0,0 +1,12 @@
+#include "LinkedList.h"
+#include "Stack.h"
+
+using namespace std; 
+
+void dispatchOrder(LinkedList &myList, string cats[]){
+	
+}
+
+void printLabel(Stack &myStack){
+	
+}
