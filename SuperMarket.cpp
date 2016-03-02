e34f9e1b9a44380c85dc304048249cf215548681
donovan521 <donovan521@users.noreply.github.com>
2016-03-02 18:47:20 -0500

    Updated

--- SuperMarket.cpp
+++ SuperMarket.cpp
@@ -1,6 +1,7 @@
 #include "LinkedList.h"
 #include "Stack.h"
-
+#include <iostream>
+#include <fstream>
 using namespace std; 
 
 void dispatchOrder(LinkedList &myList, string cats[]){
@@ -8,5 +9,19 @@ void dispatchOrder(LinkedList &myList, string cats[]){
 }
 
 void printLabel(Stack &myStack){
-	
+	 //Pop order off
+  /*
+  ofstream myfile;
+  myfile.open("output.txt");
+  for(int i=0;i<numbertodispatch;i++{
+  myfile <<"------------------------------ \n";
+  myfile <<"Order Number: " <<uniqueordernumber<<"\n";
+  myfile <<"Name: "<<ordernanme<<"\n";
+  myfile <<"Item: "<<itemname<<"\n";
+  myfile <<"Ship To: "<<location<<"\n";
+}
+  myfile <<"------------------------------ \n";
+
+  myfile.close();
+  */
 }
