7b81d6f6fb9b7afcc80cafcc7d02eb5280b5b1d5
donovan521 <donovan521@users.noreply.github.com>
2016-03-02 18:45:10 -0500

    Updated

--- p1.cpp
+++ p1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
 #include <cerrno>
 #include <string>
 #include <sstream>
@@ -7,7 +8,7 @@
 #include <unistd.h>
 #include <fstream>
 #include "Order.h"
-
+#include "LinkedQueue.h"
 using namespace std;
 
 int main(const int argc, const char * argv []) {
@@ -25,16 +26,19 @@ int main(const int argc, const char * argv []) {
   string ordersArray[100];
   string dispatchArray[100];
   int flag =0;
-  int i = 0, i2 = 0, i3 =0;
+  int i1 = 0, i2 = 0, i3 =0;
   for(string stringIn; getline(input,stringIn);) { //go thru each line
 
 
     if ((stringIn != "CATEGORIES") && (flag ==0)&&(stringIn != "ORDERS")){
-      catArray[i] = stringIn;
-      i++;
+      catArray[i1] = stringIn;
+      i1++;
+      //LinkedQueue * newQueue = new Queue(stringIn);
+
+      //make a reference to it from the linked list
     }
     if(stringIn == "ORDERS"){
-      flag++;
+    flag++;
     }
     if ((stringIn != "ORDERS") && (flag ==1)&&(stringIn != "CATEGORIES")&&(stringIn != "DISPATCH")){
       ordersArray[i2] = stringIn;
@@ -52,22 +56,33 @@ int main(const int argc, const char * argv []) {
 
 
   } // for
-//  int oNum=1;
+
+  for(int i =0;i<i3;i++){
+    istringstream iss(dispatchArray[i]);
+    string uinput;
+    int iii=0;
+    string dispatch[2];
+    while(getline(iss, uinput, ' ')){
+      dispatch[iii] = uinput;
+      iii++;
+    }
+    //store # of orders to be dispatched on linked list
+  }
+  int oNum=1;
   for(int i =0;i<i2;i++){
     istringstream iss(ordersArray[i]);
     string uinput;
     int ii=0;
-    string order2[5];
+    string order2[4];
     while(getline(iss, uinput, ',')){
       order2[ii] = uinput;
+  ii++;
     }
-    /*
-    Order newOrder(oNum,order2[0],order2[1],order2[2],order2[3],order2[4]);
-    put order in que
-    then delete it
+
+    Order * newOrder = new Order(oNum,order2[0],order2[1],order2[2],order2[3]);
     oNum++;
 
-    */
+
   }
 
   return EXIT_SUCCESS;
