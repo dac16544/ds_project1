e33201492a716627e30c58b6a14a8814ca19858b
donovan521 <donovan521@users.noreply.github.com>
2016-03-04 17:06:58 -0500

    Updated

--- LinkedStack.h
+++ LinkedStack.h
@@ -1,15 +1,20 @@
 #ifndef _LINKEDSTACK_H
 #define _LINKEDSTACK_H
-
-
 #include <string>
 #include "Order.h"
 #include "Stack.h"
+#include "LinkedList.h"
+
 using namespace std;
 
 class LinkedStack : public Stack {
 public:
-	
+  Order stackList[64];
+  int size;
+  Order toPop;
+
+   LinkedStack();
+   // ~LinkedStack();
     void push(Order & order);
 
     /**
@@ -19,7 +24,5 @@ public:
     Order & pop(void);
 
 };
-
-
 
 #endif
