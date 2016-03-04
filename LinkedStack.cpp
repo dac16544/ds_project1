122bbec3f65bfb7c6a7a26ac308f1005deb357a5
donovan521 <donovan521@users.noreply.github.com>
2016-03-04 17:07:53 -0500

    Updated

--- LinkedStack.cpp
+++ LinkedStack.cpp
@@ -1,17 +1,34 @@
 #include <string>
 #include "Order.h"
 #include "LinkedStack.h"
-	
+#include "Stack.h"
+#include <cstdlib>
+#include <iostream>
 using namespace std;
 
+Stack::~Stack(void){
+}
+
+LinkedStack::LinkedStack(){
+}
+
 void LinkedStack::push(Order & order){
+  for(int i =0; i<size;i++){
+    stackList[i+1] = stackList[i];
+  }
+  stackList[0] = order;
+  size++;
+
 
 }
 
 
 Order & LinkedStack::pop(void){
-	 Order h;
-     return h;
+  toPop = stackList[0];
+  for(int i = 1; i<size;i++){
+    stackList[i-1] = stackList[i];
+  }
+  return toPop;
 }
 
 
